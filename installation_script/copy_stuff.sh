59eb4d0c12cb4d619f989af5670926cb8acb3fce
Ivan <Ivan>
2015-12-09 00:16:12 -0800

    New setup script that accesses the newly flashed Edison

--- installation_script/copy_stuff.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script copies a bunch of files over to the Edison's home folder
-# at /home/edison/, and writes a line to rc.local, where it will be run on
-# boot (that script in rc.local  then calls all the other setup scripts 
-# before disabling itself so that it only runs once).
-
-scp setup_local.sh 
