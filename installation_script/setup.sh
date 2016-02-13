23cd3b8e35a70e86bd99ffb7bbb54daaf67dd511
Ivan <Ivan>
2016-02-12 23:47:59 -0800

    Fully working expect integration to configure root access to fresh install

--- installation_script/setup.sh
+++ installation_script/setup.sh
@@ -17,31 +17,24 @@ echo "I'm afraid you're going to have to give me root access if you want me to f
 
 sudo -p 'Password for user %u: ' echo $start
 
-# Need DFU-Util and Screen to flash and connect to the Edison
+# Need DFU-Util to flash the Edison
 if ! type "dfu-util" > /dev/null; then
   sudo apt-get install dfu-util
+  #TODO if we're not on a Debian system, inform user of need for dfu-util
 fi
 
 if ! type "screen" > /dev/null; then
   sudo apt-get install screen
+  #TODO if we're not on a Debian system, inform user of utility of Screen
 fi
 
 echo launching the ubilinux install script
 sudo ./download_ubilinux_and_flash_the_edison.sh
 echo ubilinux install script finished and we have waited 2 minutes
-# We need Expect to be installed on the host machine to interact with 
-# some of the installation scripts (and maybe to respond to the password
-# challenge from the newly flashed Edison (password "edison") to install
-# the ssh keys needed to get root access later.  Check if Expect is 
-# already installed, and if not install it.
-
-if ! type "expect" > /dev/null; then
-  sudo apt-get install expect
-fi
 
 # remove any previous Edison from the known_hosts file on the host
 # to avoid the host refusing to connect via ssh to the unfamiliar Edison
-echo removing 
+echo removing any previous Edison from Known Hosts
 ssh-keygen -f "$HOME/.ssh/known_hosts" -R 192.168.2.15
 
 # Check if there's already a key in the file $HOME/.ssh/edison on the host
@@ -52,20 +45,16 @@ if [ ! -f $key_file ]; then
     ssh-keygen -t dsa -f $key_file -P ''
     echo created an ssh key on this host computer
 fi
-pubkey=$(cat $key_file.pub)
 
-# Add ssh key pair so that we can do other stuff on the Edison as root
-# without constantly needing the user to respond to password challenges
-echo launching expect script that will answer password challenge on Edison
-expect answer_password_challenge.exp
+if ! type "expect" > /dev/null; then
+  sudo apt-get install expect
+  # TODO make this work on Mac. Probably involves figuring out where Expect
+  # binary is (I think it's pre-installed on Mac)
+fi
 
-echo 'Attempting to place an ssh key on the Edison to allow root access.'
-do_on_edison <<< "
-mkdir -p .ssh
-echo '$pubkey' >> .ssh/authorized_keys &&
-    echo 'Added ssh access to $target successfully.'
-"
-
+echo 'Attempting to place an ssh key on the Edison and set the root password.'
+pub_key=$(cat $key_file.pub)
+expect access_setup.exp $server_root_password $pub_key
 
 # Configure the Edison to get on the internet using the local wifi
 echo 'Now setting up internet access on the Edison using your wifi network.'
@@ -80,7 +69,7 @@ if [ ! -f /etc/network/interfaces.bak ]; then
   sed -i '0,/\#auto wlan0/s/\#auto wlan0/auto wlan0/' /etc/network/interfaces
   sed -i '/wpa-ssid/c\    wpa-ssid $wifi_ssid' /etc/network/interfaces
   sed -i '/wpa-psk/c\    wpa-psk $wifi_password' /etc/network/interfaces
-  sed -i '/auto usb0/c\\#auto usb0' /etc/network/interfaces
+  # sed -i '/auto usb0/c\\#auto usb0' /etc/network/interfaces
 fi
 
 echo changing the hostname of the server
@@ -120,21 +109,8 @@ copy_to_edison toCopy/create_db_and_user.sql /home/edison/scripts/files/
 copy_to_edison toCopy/index.html /home/edison/scripts/files/
 
 
-echo preparing to run the server setup script on reboot
-copy_to_edison toCopy/kickoff.sh /home/edison/scripts/
-#
-# TODO apparently mucking with rc.local causes a fatal reboot loop. Bork.
-# Need to find another way to run the kickoff script.
-#
-# Add a line to the rc.local file to run a setup script next boot.
-# do_on_edison <<< "
-# chmod +x /home/edison/scripts/kickoff.sh
-# sed -i '/exit 0/c\\/home\/edison\/scripts\/kickoff.sh\n\nexit 0' /etc/rc.local
-# "
-
-
-
-echo NOT rebooting the Edison and hope that the kickoff script works
-#do_on_edison <<< "
-#reboot
-#"
+echo rebooting the Edison and wishing we had set up a startup service
+sleep 5
+do_on_edison <<< "
+reboot
+"
